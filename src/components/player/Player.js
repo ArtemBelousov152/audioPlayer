ef06ed620acdfe273023aa56e643edc7ac6d79c5
Vizeyris <zopash1@Mail.ru>
2023-02-08 18:43:08 +0300

    v1

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -63,6 +63,8 @@ export default function Player() {
 
         if (!isNaN(time) && time !== Infinity) {
             setProgress(time);
+        } else {
+            setProgress(100);
         }
 
 
