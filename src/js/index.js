2da6c5e409395bdcd1c8a3a36a00559c383104c7
Vizeyris <zopash1@Mail.ru>
2023-02-23 21:57:35 +0300

    mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,10 @@ import reactStart from '../../react/src/index';
 import technicalSwitch from './modules/technialSwitch';
 import closeError from './modules/closeError';
 
-import '../style/style.scss';
-
-
 window.addEventListener('DOMContentLoaded', () => {
     reactStart();
     technicalSwitch();
     closeError();
 })
+
+import '../style/style.scss';
